57d50892bbbe50a02b46d2706fa9d558de950dad
Seth Kerr <thesethkerr@gmail.com>
2020-07-20 15:06:46 -0400

    adding my registration changes

--- plugins.js
+++ plugins.js
@@ -6,6 +6,7 @@ $(() => {
     if (TOKEN_KEY === " " || NAME_KEY === " ") {
         localStorage.setItem(TOKEN_KEY, " ")
         localStorage.setItem(NAME_KEY, " ")
+        localStorage.setItem(USER_KEY, " ")
     }
     $('#advisor_signup').click(() => {
             if ($('#inputPassword').val() === $('#inputRPassword').val()) {
@@ -19,6 +20,17 @@ $(() => {
             }
         }
     )
+    $('#standard_signup').click(() => {
+        if ($("#inputPassword").val() === $("#inputRPassword").val()) {
+            const user = {
+                firstName: $("#inputFirstName").val(),
+                lastName: $("#inputLastName").val(),
+                email: $("#inputEmail").val(),
+                password: $("#inputPassword").val(),
+                accountType: "Basic User"
+            }
+        }
+    })
 })
 
 function authenticate(res) {
@@ -31,4 +43,10 @@ function authenticate(res) {
     console.log(JSON.stringify(res.name.name))
     localStorage.setItem(TOKEN_KEY, res.token.toString())
     localStorage.setItem(NAME_KEY, JSON.stringify(res.name))
+    localStorage.setItem(USER_KEY, JSON.stringify(res.accountType))
+}
+function register(user) {
+    $.post(userurl + "auth/st_register", user, (data) => {
+        authenticate(data)
+    })
 }
