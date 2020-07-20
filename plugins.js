4cb2254db451cad87a633c87f4182588d82f5842
Seth Kerr <thesethkerr@gmail.com>
2020-07-20 14:49:17 -0400

    adding standard registration stuff

--- plugins.js
+++ plugins.js
@@ -3,10 +3,22 @@ let socket = io()
 const userurl = window.location.href
 
 $(() => {
-    if(TOKEN_KEY === " " || NAME_KEY === " ") {
+    if(TOKEN_KEY === " " || NAME_KEY === " " || USER_KEY) {
         localStorage.setItem(TOKEN_KEY, " ")
         localStorage.setItem(NAME_KEY, " ")
+        localStorage.setItem(USER_KEY, " ")
     }
+    $('#standard_signup').click(() => {
+            if ($("#inputPassword").val() === $("#inputRPassword").val()) {
+                const user = {
+                    firstName: $("#inputFirstName").val(),
+                    lastName: $("#inputLastName").val(),
+                    email: $("#inputEmail").val(),
+                    password: $("#inputPassword").val(),
+                    accountType: "Basic User"
+                }
+            }
+        })
 
 })
 
@@ -20,4 +32,11 @@ function authenticate(res) {
     console.log(JSON.stringify(res.name.name))
     localStorage.setItem(TOKEN_KEY, res.token.toString())
     localStorage.setItem(NAME_KEY, JSON.stringify(res.name))
+    localStorage.setItem(USER_KEY, JSON.stringify(res.accountType))
+}
+
+function register(user) {
+    $.post(userurl + "auth/st_register", user, (data) => {
+        authenticate(data)
+    })
 }
