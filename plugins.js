8374e0693f48825550f6ef52afc6f374c0babe48
Seth Kerr <thesethkerr@gmail.com>
2020-07-20 14:58:00 -0400

    try again

--- plugins.js
+++ plugins.js
@@ -3,23 +3,34 @@ let socket = io()
 const userurl = window.location.href
 
 $(() => {
-    if(TOKEN_KEY === " " || NAME_KEY === " " || USER_KEY) {
+    if (TOKEN_KEY === " " || NAME_KEY === " ") {
         localStorage.setItem(TOKEN_KEY, " ")
         localStorage.setItem(NAME_KEY, " ")
         localStorage.setItem(USER_KEY, " ")
     }
-    $('#standard_signup').click(() => {
-            if ($("#inputPassword").val() === $("#inputRPassword").val()) {
-                const user = {
-                    firstName: $("#inputFirstName").val(),
-                    lastName: $("#inputLastName").val(),
-                    email: $("#inputEmail").val(),
-                    password: $("#inputPassword").val(),
-                    accountType: "Basic User"
+    $('#advisor_signup').click(() => {
+            if ($('#inputPassword').val() === $('#inputRPassword').val()) {
+                const advisorUser = {
+                    firstName: $('#inputFirstName').val(),
+                    lastName: $('#inputLastName').val(),
+                    email: $('#inputEmail').val(),
+                    password: $('#inputPassword').val(),
+                    accountType: 'Financial Advisor'
                 }
             }
-        })
-
+        }
+    )
+    $('#standard_signup').click(() => {
+        if ($("#inputPassword").val() === $("#inputRPassword").val()) {
+            const user = {
+                firstName: $("#inputFirstName").val(),
+                lastName: $("#inputLastName").val(),
+                email: $("#inputEmail").val(),
+                password: $("#inputPassword").val(),
+                accountType: "Basic User"
+            }
+        }
+    })
 })
 
 function authenticate(res) {
@@ -34,7 +45,6 @@ function authenticate(res) {
     localStorage.setItem(NAME_KEY, JSON.stringify(res.name))
     localStorage.setItem(USER_KEY, JSON.stringify(res.accountType))
 }
-
 function register(user) {
     $.post(userurl + "auth/st_register", user, (data) => {
         authenticate(data)
